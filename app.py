2804a8f60ccf3839a0400e0fa60ea41753cf5066
sanjaybhide1990@gmail.com <sanjaybhide1990@gmail.com>
2023-04-26 21:14:08 +0530

    Added library to handle env variables

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os, uuid
 from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Sanjay123@localhost/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
